e0f00699d58ac153254b885aabaae4e70f13ecbf
dianahong100 <dianahong2424@gmail.com>
2025-10-29 19:43:38 -0400

    chart built

--- js/chartmean.js
+++ js/chartmean.js
@@ -7,10 +7,11 @@ async function getData(){
    console.log(data);
 
 
-   const xGenre = [];          // x-axis label = genres
-   const yMean = [];          // y-axis global temp values
-   const yNHtemps = [];        // y-axis NH temp values
-   const ySHtemps = [];        // y - axis SH temp values
+   const genres = [];          // genres
+   const mean = [];          // mean accuracy values
+   const standDev = [];        // standard deviation
+   const variation = [];        // variation
+   const trials = [];          // number of trials
 
 
    // \n - new line character
@@ -24,137 +25,127 @@ async function getData(){
 
    table.forEach(row => {
        const columns = row.split(',')
-       const genre = parseFloat(columns[0]);        // Assign year value
-       xGenre.push(genre);                          // Push each year into array for years
+       const genre = columns[0];        // Assign genres value
+       genres.push(genre);                          // Push each genre into array
 
 
-       const temp = parseFloat(columns[1]);        // Convert global temp. to float
-       yMean.push(temp + 14);                     // Push temp values to array and refrence to 0-deg. C
+       const avg = parseFloat(columns[1]);        // Convert mean to float
+       mean.push(avg);                     // Push mean values to array
 
+       const stdv = parseFloat(columns[2]);     /// convert standard deviation to float
+       standDev.push(stdv);                // push standard dev values to array
 
-       const nhTemp = parseFloat(columns[2]);        // Convert NH temp. to float
-       yNHtemps.push(nhTemp + 14);                     // Push temp values to array and refrence to 0-deg. C
+       const variate = parseFloat(columns[3]);  // convert variation to float
+       variation.push(variate);         // push variation values to array
 
+       const trialNum = parseFloat(columns[4]);   // convert number of trials to float
+       trials.push(trialNum);           // push number of trials to array
 
-       const shTemp = parseFloat(columns[3]);        // Convert SH temp. to float
-       ySHtemps.push(shTemp + 14);                     // Push temp values to array and refrence to 0-deg. C
-
-
-       console.log(year, temp, nhTemp, shTemp);
+       console.log(genre, avg, stdv, variate, trialNum);
    });
 
 
-   return {xYears, yTemps, yNHtemps, ySHtemps} //Use {} to return multiple values of a single object
+   return {genres, mean, standDev, variation, trials} //Use {} to return multiple values of a single object
 }
 
 
 async function createChart(){
    const data = await getData();       // createChart will wait for getData() to process
    const radarChart = document.getElementById('hongRadarChart');
-   const degreeSymbol = String.fromCharCode(176);
-
 
    const myChart = new Chart(radarChart, {  // Construct the chart   
-       type: 'line',
+       type: 'bar',
        data: {                         // Define data
-           labels: data.xYears,        // x-axis labels
+           labels: [
+            'Mean',
+            'Standard Deviation',
+            'Variation',
+            'Number of Trials'
+           ],        // labels
            datasets: [                 // Each object describes one dataset of y-values
                                        //  including display properties.  To add more datasets,
                                        //  place a comma after the closing curly brace of the last
                                        //  data set object and add another dataset object.
                {
-                   label:    `Combined Global LSA and SSW Temperature in ${degreeSymbol}C`,     // Dataset label for legend
-                   data:     data.yTemps,    // Reference to array of y-values
+                   label:    'Grunge',     // grunge genre
+                   data:     [data.mean[0], data.standDev[0], data.variation[0], data.trials[0]],    // Reference to array
                    fill:     false,           // Fill area under the linechart (true = yes, false = no)
-                   backgroundColor:  'rgba(255, 0, 132, 0.2)',    // Color for data marker
-                   borderColor:      'rgba(255, 0, 132, 1)',      // Color for data marker border
-                   borderWidth:      1   // Data marker border width
+                   backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                   borderColor: 'rgb(255, 99, 132)',
+                   pointBackgroundColor: 'rgb(255, 99, 132)',
+                   pointBorderColor: '#fff',
+                   pointHoverBackgroundColor: '#fff',
+                   pointHoverBorderColor: 'rgb(255, 99, 132)'
                },
                {
-                   label:    `Combined Global LSA and SSW Temperature in ${degreeSymbol}C`,     // Dataset label for legend
-                   data:     data.yNHtemps,    // Reference to array of y-values
+                   label:    'Rock',     // grunge genre
+                   data:     [data.mean[1], data.standDev[1], data.variation[1], data.trials[1]],    // Reference to array
                    fill:     false,           // Fill area under the linechart (true = yes, false = no)
-                   backgroundColor:  'rgba(0, 102, 255, 0.2)',    // Color for data marker
-                   borderColor:      'rgba(0, 102, 255, 1)',      // Color for data marker border
-                   borderWidth:      1   // Data marker border width
+                   backgroundColor: 'rgba(102, 99, 255, 0.2)',
+                   borderColor: 'rgba(99, 102, 255, 1)',
+                   pointBackgroundColor: 'rgba(99, 115, 255, 1)',
+                   pointBorderColor: '#fff',
+                   pointHoverBackgroundColor: '#fff',
+                   pointHoverBorderColor: 'rgba(99, 115, 255, 1)'
                },
                {
-                   label:    `Combined Global LSA and SSW Temperature in ${degreeSymbol}`,     // Dataset label for legend
-                   data:     data.ySHtemps,    // Reference to array of y-values
+                   label:    'Shoegaze',     // Dataset label for legend
+                   data:     [data.mean[2], data.standDev[2], data.variation[2], data.trials[2]],    // Reference to array
                    fill:     false,           // Fill area under the linechart (true = yes, false = no)
-                   backgroundColor:  'rgba(0, 153, 51, 0.2)',    // Color for data marker
-                   borderColor:      'rgba(0, 153, 51, 1)',      // Color for data marker border
-                   borderWidth:      1   // Data marker border width
+                   backgroundColor: 'rgba(99, 255, 99, 0.2)',
+                   borderColor: 'rgba(99, 255, 125, 1)',
+                   pointBackgroundColor: 'rgba(99, 255, 125, 1)',
+                   pointBorderColor: '#fff',
+                   pointHoverBackgroundColor: '#fff',
+                   pointHoverBorderColor: 'rgba(99, 255, 120, 1)'
+               },
+               {
+                   label:    'Pop',     // Dataset label for legend
+                   data:     [data.mean[3], data.standDev[3], data.variation[3], data.trials[3]],    // Reference to array
+                   fill:     false,           // Fill area under the linechart (true = yes, false = no)
+                   backgroundColor: 'rgba(255, 239, 99, 0.2)',
+                   borderColor: 'rgba(255, 252, 99, 1)',
+                   pointBackgroundColor: 'rgba(255, 226, 99, 1)',
+                   pointBorderColor: '#fff',
+                   pointHoverBackgroundColor: '#fff',
+                   pointHoverBorderColor: 'rgba(255, 245, 99, 1)'
+               },
+               {
+                   label:    'Control',     // Dataset label for legend
+                   data:     [data.mean[4], data.standDev[4], data.variation[4], data.trials[4]],    // Reference to array
+                   fill:     false,           // Fill area under the linechart (true = yes, false = no)
+                   backgroundColor: 'rgba(255, 156, 99, 0.2)',
+                   borderColor: 'rgba(255, 182, 99, 1)',
+                   pointBackgroundColor: 'rgba(255, 169, 99, 1)',
+                   pointBorderColor: '#fff',
+                   pointHoverBackgroundColor: '#fff',
+                   pointHoverBorderColor: 'rgba(255, 161, 99, 1)'
                }
        ]
        },
-       options: {                        // Define display chart display options
-           responsive: true,             // Re-size based on screen size
-           maintainAspectRatio: false,
-           scales: {                     // Display options for x & y axes
-               x: {                      // x-axis properties
-                   title: {
-                       display: true,
-                       text: 'Year',     // x-axis title
-                       font: {                   // font properties
-                           size: 14
-                       },
-                   },
-                   ticks: {                      // x-axis tick mark properties
-                       callback: function(val, index) {
-                           //Set the tick marks at every 5 years
-                           return index %5 === 0 ? this.getLabelForValue(val) : ``;
-                       },
-                   font: {
-                       size: 14 
-                       },
-                   },
-                   grid: {                       // x-axis grid properties
-                       color: '#6c767e'
-                   }
-               },
-               y: {                              // y-axis properties
-                   title: {
-                       display: true,                         
-                       text: `Global Mean Temperatures (${degreeSymbol}C)`,     // y-axis title
-                       font: {
-                           size: 14
-                       },
-                   },
-                   ticks: {
-                       min: 0,                  
-                       maxTicksLimit: data.yTemps.length/10,        // Actual value can be set dynamically
-                       font: {
-                           size: 12
-                       }
-                   },
-                   grid: {                       // y-axis gridlines
-                       color: '#6c767e'
-                   }
-               }
-           },
-           plugins: {                  // Display options for title and legend
-               title: {
-                   display: true,
-                   text: 'Global Mean Temperature vs. Year (since 1880)',
-                   font: {
-                       size: 24,
-                   },
-                   color: '#black',
-                   padding: {
-                       top: 10,
-                       bottom: 30
-                   }
-               },
-               legend: {
-                   align: 'start',
-                   position: 'bottom',
-               }
-           }
-       }      
+       options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Memorization Accuracy by Genre of Music',
+                font: {
+                    size: 24
+                },
+                padding: {
+                    top: 10,
+                    bottom: 30
+                }
+            },
+            legend: {
+                align: 'start',
+                position: 'bottom'
+            }
+        }
+       }
    });
 }
 
 
 createChart();
-
